5a57a830fd0880323233c3e1d191cb2105cbb767
Ju <94855929+PrincesLu@users.noreply.github.com>
2022-10-24 20:28:37 +0100

    Add files via upload

---
+++ week4.java
@@ -0,0 +1,17 @@
+package week4;
+
+import java.util.Scanner;
+
+public class week4 {
+	public static void main( String[] args) {
+		Scanner scanner = new Scanner(System.in);
+//		String x = scanner.nextLine();
+//		System.out.println(x); 
+		int y = scanner.nextInt(); 
+		System.out.println( y);
+		int a = y + 10; 
+		System.out.println(a);
+		
+	}
+
+}
